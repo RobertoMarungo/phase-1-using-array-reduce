d53eddebeaa8ee11d976f0191a1986b8fc6bd62a
Roberto Marungo <robmarungo.dev@gmail.com>
2023-05-06 01:08:20 -0500

    Refactored Code

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 // Code your solution here
 
 const totalBatteries = batteryBatches.reduce(
-  (value, battBatches) => battBatches + value
+  (value, batch) => batch + value,
+  0
 );
